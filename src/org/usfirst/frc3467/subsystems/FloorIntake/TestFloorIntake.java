0c52371b5dd5dfc5e838468657a35b63264c10e9
scottkukshtel <scottkukshtel@team3467.org>
2017-03-01 15:35:27 -0500

    Floor Intake fixups

--- src/org/usfirst/frc3467/subsystems/FloorIntake/TestFloorIntake.java
+++ src/org/usfirst/frc3467/subsystems/FloorIntake/TestFloorIntake.java
@@ -1,18 +1,18 @@
 package org.usfirst.frc3467.subsystems.FloorIntake;
 
 import org.usfirst.frc3467.robot.CommandBase;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
-public class ToggleFloorIntakeOperation extends CommandBase {
+public class TestFloorIntake extends CommandBase {
 	
-	public static boolean isIn = true;
+	double speed;
 
-    public ToggleFloorIntakeOperation() {
-        requires(flr_intake);
+    public TestFloorIntake() {
+    	requires (flr_intake);
+    	SmartDashboard.putNumber("Set Floor Intake Speed", .1);
     }
 
     // Called just before this Command runs the first time
@@ -20,17 +20,9 @@ public class ToggleFloorIntakeOperation extends CommandBase {
     }
 
     // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	
-    	if (isIn == true) {
-    		pneumatics.floorIntakeExtend();
-    		isIn = false;
-    	}
-    	else {
-    		pneumatics.floorIntakeRetract();
-    		isIn = true;
-    	}
-    	SmartDashboard.putString("Floor Intake Position",  isIn ? "IN" : "OUT");
+	protected void execute() {
+    	speed = SmartDashboard.getNumber("Set Floor Intake Speed", .1);
+    	flr_intake.drive(speed);
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -40,10 +32,13 @@ public class ToggleFloorIntakeOperation extends CommandBase {
 
     // Called once after isFinished returns true
     protected void end() {
+    	flr_intake.drive(0.0);
+    	
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
