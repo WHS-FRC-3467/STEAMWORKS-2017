6efc0e3bfa3e65e4d846f34ab4680fa8a6d97a59
scottkukshtel <scottkukshtel@team3467.org>
2017-03-23 16:42:13 -0400

    Work to clean up Climber code (not finished)

--- src/org/usfirst/frc3467/subsystems/Climber/AutoClimber.java
+++ src/org/usfirst/frc3467/subsystems/Climber/AutoClimber.java
@@ -4,50 +4,50 @@ import org.usfirst.frc3467.robot.CommandBase;
 
 public class AutoClimber  extends CommandBase{
 
+	int state;
+	boolean done;
 
-	int state = 0;
 	final double ANGLE_THRESHOLD = 45;
 	//2048 ticks per rev 44-34 rpm
 	final double CLIMB_HEIGHT = 5000;
 	final int TOLERANCE = 5;
 	final double CURRENT_THRESHOLD = 2;
-	boolean firstRepeat = true;
-	boolean done = false;
 	
 	public AutoClimber(){
-		requires(driveBase);
+		requires(climber);
 		requires(gyro);
 	}
 	
-	protected void initialize(){
-
+	protected void initialize() {
+		state = 0;
+		done = false;
 	}
 	
 	protected void execute(){
-		/*if(state == 0){
-			cTalon1.set(1);
-			if(firstRepeat){
-				latch.set(1);
-				firstRepeat = false;
+
+/*		switch(state) {
+		case 0:
+			climber.setLatchServo(1);
+			climber.startPIDControl();
+			state = 1;
+			break;
+		
+		case 1:
+			if(gyro.getVector()[2] > ANGLE_THRESHOLD) {
+				state = 2;
 			}
-			else if(gyro.getVector()[2] > ANGLE_THRESHOLD){
-				state = 1;
-				firstRepeat = true;
-			}
-		}
-		else if(state == 1){
-			if(firstRepeat){
-				cTalon1.enableControl();
-				cTalon1.setSetpoint(CLIMB_HEIGHT);
-				firstRepeat = false;
-			}
-			else if(cTalon1.getSetpoint()-cTalon1.getPosition() < TOLERANCE){
+			break;
+			
+		case 2:
+			cTalon1.enableControl();
+			cTalon1.setSetpoint(CLIMB_HEIGHT);
+			break;
+
+		case 3:
+			if(cTalon1.getSetpoint() - cTalon1.getPosition() < TOLERANCE){
 				cTalon1.disableControl();
 				cTalon1.set(0);
-				state = 2;
-				firstRepeat = true;
 			}
-		}
 		else if(state == 2){
 			if(firstRepeat){
 				cTalon1.set(.1);
@@ -58,9 +58,10 @@ public class AutoClimber  extends CommandBase{
 				cTalon1.setSetpoint(cTalon1.getPosition());
 				done = true;
 			}
-		} */
-		climber.setLatchServo(0);
-	}
+		}
+*/
+		}
+
 	protected boolean isFinished(){
 		return done;
 		
@@ -68,6 +69,7 @@ public class AutoClimber  extends CommandBase{
 	protected void end(){
 		
 	}
+	
 	protected void interrupted(){
 		end();
 	}
--- src/org/usfirst/frc3467/subsystems/Climber/Climber.java
+++ src/org/usfirst/frc3467/subsystems/Climber/Climber.java
@@ -13,43 +13,35 @@ import com.ctre.CANTalon;
  *
  */
 public class Climber extends Subsystem {
-    // Controls display SmartDashboard
+
+	// Controls display SmartDashboard
 	private static final boolean debugging = true;
-	//Constants for speed
-	public static final double kClimb = 0.7;
-	public static final double kLower = -0.3;
-	public static final double kStop = 0;
 
 	private Servo latchServo;
-	CANTalon cTalon1;
+	private CANTalon climbMotor;
 
-	//Roller class objects
-//	public CANTalon climbMotor;
-
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-	public Climber(){
+    // Constructor
+	public Climber() {
 
 		// Climber Latch
 		latchServo = new Servo(RobotMap.climberLatch_Servo);
 		
 		// Climber motor
-		cTalon1 = DriveBase.getInstance().getMiddleTalon();
+		climbMotor = DriveBase.getInstance().getMiddleTalon();
 
 	}
+	
     public void initDefaultCommand() {
 
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand()); 
     	this.setDefaultCommand(new ClimberDrive());
     }
-	public void driveAuto(double speed) {
-		// TODO Auto-generated method stub
-		if (debugging) {
+
+    public void driveClimber (double speed) {
+
+    	if (debugging) {
 	    	SmartDashboard.putNumber("Climbing Speed", speed);
 		}
-//		climbMotor.set(speed);
-		
+		climbMotor.set(speed);
 	}
 	
 	public void setLatchServo(double value) {
