609a0127e6ebba93e5eacc8ea9a836ff12376d40
scottkukshtel <scottkukshtel@team3467.org>
2017-04-15 12:10:09 -0400

    Updates for various driving

--- src/org/usfirst/frc3467/robot/commands/Autonomous/AutoShootLeft.java
+++ src/org/usfirst/frc3467/robot/commands/Autonomous/AutoShootLeft.java
@@ -1,6 +1,6 @@
 package org.usfirst.frc3467.robot.commands.Autonomous;
 
-import org.usfirst.frc3467.subsystems.DriveBase.DriveSideways;
+import org.usfirst.frc3467.subsystems.DriveBase.DriveSidewaysByTime;
 import org.usfirst.frc3467.subsystems.Pneumatics.ToggleIntakeRamp;
 import org.usfirst.frc3467.subsystems.Shooter.OperateShooter;
 
@@ -13,6 +13,6 @@ public class AutoShootLeft extends CommandGroup {
 
     public AutoShootLeft() {
     	addSequential(new OperateShooter(true));
-    	addSequential(new DriveSideways(2.75, 0.5));
+    	addSequential(new DriveSidewaysByTime(2.75, 0.5));
     }
 }
